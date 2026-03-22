94d90689d6577c28c5925f9bd65f6812d4301cc3
TheOriginalHatux <niklasfr1998@gmail.com>
2026-03-22 11:21:52 +0100

    Initial ASP.NET Core project with Docker support

---
+++ HatuxDevLab.Web/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HatuxDevLab.Web.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
